7e1054338c49ceee30538679e6750708eff4fd7e
stonio <stonio@users.noreply.github.com>
2016-06-24 14:49:52 +0200

    Functions - Restore import

--- jstl/src/main/java/com/github/slugify/Functions.java
+++ jstl/src/main/java/com/github/slugify/Functions.java
@@ -1,5 +1,7 @@
 package com.github.slugify;
 
+import java.io.IOException;
+
 public class Functions {
 	public static String slugify(String input) throws IOException {
 		return InitSlugifyTag.getSlugify().slugify(input);
