3e3b365b4f5f2c42bfa5d9e28afc2702f4e8a31d
Danny Trunk <dtrunk90@gmail.com>
2022-06-01 21:07:27 +0200

    Initial commit

---
+++ src/test/java/com/github/slugify/NormalizeTests.java
@@ -0,0 +1,82 @@
+package com.github.slugify;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NormalizeTests {
+    private final Slugify slugify = Slugify.builder().build();
+
+    @Test
+    public void shouldReturnSlugifiedString() {
+        // given
+        final String text = "Hello, world!";
+
+        // when
+        final String result = slugify.slugify(text);
+
+        // then
+        assertEquals("hello-world", result);
+    }
+
+    @Test
+    public void shouldTrimWhiteSpacesOtherThanSpace() {
+        // given
+        String string = "\tHello \tworld \r\t";
+
+        // when
+        String result = slugify.slugify(string);
+
+        // then
+        assertEquals("hello-world", result);
+    }
+
+
+    @Test
+    public void shouldSlugifyAnyPrintableASCIICharacter() {
+        // given
+        String string = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+        // when
+        String result = slugify.slugify(string);
+
+        // then
+        assertEquals("0123456789-abcdefghijklmnopqrstuvwxyz-_-abcdefghijklmnopqrstuvwxyz", result);
+    }
+
+    @Test
+    public void shouldReplacePlusSignToSeparator() {
+        // given
+        String string = "\tHello+\tworld \r\t";
+
+        // when
+        String result = slugify.slugify(string);
+
+        // then
+        assertEquals("hello-world", result);
+    }
+
+    @Test
+    public void shouldReturnEmptyStringIfNullGiven() {
+        // given
+        String string = null;
+
+        // when
+        String result = slugify.slugify(string);
+
+        // then
+        assertEquals("", result);
+    }
+
+    @Test
+    public void shouldNormalizeRepeatedHyphensToSingleHyphenWithHyphenSeparator() {
+        //given
+        String string = "a---b___c";
+
+        //when
+        String result = slugify.slugify(string);
+
+        //then
+        assertEquals("a-b___c", result);
+    }
+}
