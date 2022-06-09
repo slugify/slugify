8675225a7290260bc28549bf5625727ffcf9a958
Danny Trunk <dtrunk90@gmail.com>
2022-06-09 22:55:21 +0200

    checkstyle: Add plugin and fix errors

--- src/main/java/com/github/slugify/Slugify.java
+++ src/main/java/com/github/slugify/Slugify.java
@@ -1,90 +1,110 @@
 package com.github.slugify;
 
 import com.ibm.icu.text.Transliterator;
-import lombok.Builder;
-import lombok.Singular;
-
 import java.text.Normalizer;
-import java.util.*;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.ResourceBundle;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import lombok.Builder;
+import lombok.Singular;
 
+/**
+ * Class for generating SEO-friendly URLs.
+ *
+ * @author Danny Trunk
+ * @since 1.0
+ */
 public class Slugify {
-    private static final String ASCII = "Cyrillic-Latin; Any-Latin; Latin-ASCII; [^\\p{Print}] Remove; ['\"] Remove; Any-Lower";
-    private static final String BUNDLE_BASE_NAME = "slugify";
-    private static final String EMPTY = "";
-    private static final String UNDERSCORE = "_";
-    private static final String HYPHEN = "-";
+  private static final String ASCII =
+      "Cyrillic-Latin; Any-Latin; Latin-ASCII; [^\\p{Print}] Remove; ['\"] Remove; Any-Lower";
+  private static final String BUNDLE_BASE_NAME = "slugify";
+  private static final String EMPTY = "";
+  private static final String UNDERSCORE = "_";
+  private static final String HYPHEN = "-";
 
-    private static final Pattern PATTERN_NORMALIZE_NON_ASCII = Pattern.compile("[^\\p{ASCII}]+");
-    private static final Pattern PATTERN_NORMALIZE_HYPHEN_SEPARATOR = Pattern.compile("[\\W\\s+]+");
-    private static final Pattern PATTERN_NORMALIZE_UNDERSCORE_SEPARATOR = Pattern.compile("[[^a-zA-Z0-9\\-]\\s+]+");
-    private static final Pattern PATTERN_NORMALIZE_TRIM_DASH = Pattern.compile("^-|-$");
+  private static final Pattern PATTERN_NORMALIZE_NON_ASCII = Pattern.compile("[^\\p{ASCII}]+");
+  private static final Pattern PATTERN_NORMALIZE_HYPHEN_SEPARATOR = Pattern.compile("[\\W\\s+]+");
+  private static final Pattern PATTERN_NORMALIZE_UNDERSCORE_SEPARATOR =
+      Pattern.compile("[[^a-zA-Z0-9\\-]\\s+]+");
+  private static final Pattern PATTERN_NORMALIZE_TRIM_DASH = Pattern.compile("^-|-$");
 
-    private final boolean transliterator;
-    private final boolean underscoreSeparator;
-    private final boolean lowerCase;
+  private final boolean transliterator;
+  private final boolean underscoreSeparator;
+  private final boolean lowerCase;
 
-    private final Locale locale;
+  private final Locale locale;
 
-    private final Map<String, String> customReplacements;
-    private final Map<String, String> replacements;
+  private final Map<String, String> customReplacements;
+  private final Map<String, String> replacements;
 
-    @Builder
-    public Slugify(final Boolean transliterator, final Boolean underscoreSeparator, final Boolean lowerCase, final Locale locale, @Singular final Map<String, String> customReplacements) {
-        this.transliterator = Optional.ofNullable(transliterator).orElse(false);
-        this.underscoreSeparator = Optional.ofNullable(underscoreSeparator).orElse(false);
-        this.lowerCase = Optional.ofNullable(lowerCase).orElse(true);
+  @Builder
+  protected Slugify(final Boolean transliterator, final Boolean underscoreSeparator,
+                 final Boolean lowerCase, final Locale locale,
+                 @Singular final Map<String, String> customReplacements) {
+    this.transliterator = Optional.ofNullable(transliterator).orElse(false);
+    this.underscoreSeparator = Optional.ofNullable(underscoreSeparator).orElse(false);
+    this.lowerCase = Optional.ofNullable(lowerCase).orElse(true);
 
-        this.locale = Optional.ofNullable(locale).orElseGet(Locale::getDefault);
+    this.locale = Optional.ofNullable(locale).orElseGet(Locale::getDefault);
 
-        this.customReplacements = Optional.ofNullable(customReplacements).orElseGet(Collections::emptyMap);
+    this.customReplacements = Optional.ofNullable(customReplacements)
+        .orElseGet(Collections::emptyMap);
 
-        ResourceBundle replacementsBundle = ResourceBundle.getBundle(BUNDLE_BASE_NAME, this.locale);
-        this.replacements = replacementsBundle.keySet().stream()
-                .collect(Collectors.toMap(Function.identity(), replacementsBundle::getString));
-    }
+    ResourceBundle replacementsBundle = ResourceBundle.getBundle(BUNDLE_BASE_NAME, this.locale);
+    this.replacements = replacementsBundle.keySet().stream()
+        .collect(Collectors.toMap(Function.identity(), replacementsBundle::getString));
+  }
 
-    public String slugify(final String text) {
-        return Optional.ofNullable(text)
-                // remove leading and trailing whitespaces
-                .map(String::trim)
-                // run subsequent calls only if string is not empty
-                .filter(Predicate.not(EMPTY::equals))
-                // replace all custom replacements
-                .map(str -> replaceAll(str, customReplacements))
-                // replace all built-in replacements
-                .map(str -> replaceAll(str, replacements))
-                // transliterate or normalize
-                .map(str -> transliterator ? transliterate(str) : normalize(str))
-                // remove all remaining non ascii chars
-                .map(str -> PATTERN_NORMALIZE_NON_ASCII.matcher(str).replaceAll(EMPTY))
-                // replace remaining chars matching a pattern with underscore/hyphen
-                .map(str -> underscoreSeparator
-                        ? PATTERN_NORMALIZE_UNDERSCORE_SEPARATOR.matcher(str).replaceAll(UNDERSCORE)
-                        : PATTERN_NORMALIZE_HYPHEN_SEPARATOR.matcher(str).replaceAll(HYPHEN))
-                // remove leading and trailing dashes
-                .map(str -> PATTERN_NORMALIZE_TRIM_DASH.matcher(str).replaceAll(EMPTY))
-                // convert to lower case if needed
-                .map(str -> lowerCase ? str.toLowerCase(locale) : str)
-                // return empty string if input is null or empty
-                .orElse(EMPTY);
-    }
+  /**
+   * Creates a slug from the specified text.
+   *
+   * @param text Text to create a slug from.
+   * @return A string representing the slug.
+   */
+  public String slugify(final String text) {
+    return Optional.ofNullable(text)
+        // remove leading and trailing whitespaces
+        .map(String::trim)
+        // run subsequent calls only if string is not empty
+        .filter(Predicate.not(EMPTY::equals))
+        // replace all custom replacements
+        .map(str -> replaceAll(str, customReplacements))
+        // replace all built-in replacements
+        .map(str -> replaceAll(str, replacements))
+        // transliterate or normalize
+        .map(str -> transliterator ? transliterate(str) : normalize(str))
+        // remove all remaining non ascii chars
+        .map(str -> PATTERN_NORMALIZE_NON_ASCII.matcher(str).replaceAll(EMPTY))
+        // replace remaining chars matching a pattern with underscore/hyphen
+        .map(str -> underscoreSeparator
+            ? PATTERN_NORMALIZE_UNDERSCORE_SEPARATOR.matcher(str).replaceAll(UNDERSCORE)
+            : PATTERN_NORMALIZE_HYPHEN_SEPARATOR.matcher(str).replaceAll(HYPHEN))
+        // remove leading and trailing dashes
+        .map(str -> PATTERN_NORMALIZE_TRIM_DASH.matcher(str).replaceAll(EMPTY))
+        // convert to lower case if needed
+        .map(str -> lowerCase ? str.toLowerCase(locale) : str)
+        // return empty string if input is null or empty
+        .orElse(EMPTY);
+  }
 
-    private String replaceAll(final String input, Map<String, String> replacements) {
-        return replacements.keySet().stream()
-                .map(key -> (Function<String, String>) str -> str.replace(key, replacements.get(key)))
-                .reduce(Function.identity(), Function::andThen)
-                .apply(input);
-    }
+  private String replaceAll(final String input, Map<String, String> replacements) {
+    return replacements.keySet().stream()
+        .map(key -> (Function<String, String>) str -> str.replace(key, replacements.get(key)))
+        .reduce(Function.identity(), Function::andThen)
+        .apply(input);
+  }
 
-    private String transliterate(final String input) {
-        return Transliterator.getInstance(ASCII).transliterate(input);
-    }
+  private String transliterate(final String input) {
+    return Transliterator.getInstance(ASCII).transliterate(input);
+  }
 
-    private String normalize(final String input) {
-        return Normalizer.normalize(input, Normalizer.Form.NFKD);
-    }
+  private String normalize(final String input) {
+    return Normalizer.normalize(input, Normalizer.Form.NFKD);
+  }
 }
--- src/test/java/com/github/slugify/NormalizeTests.java
+++ src/test/java/com/github/slugify/NormalizeTests.java
@@ -1,82 +1,88 @@
 package com.github.slugify;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+/**
+ * Class for testing normalization.
+ *
+ * @author Danny Trunk
+ */
 public class NormalizeTests {
-    private final Slugify slugify = Slugify.builder().build();
+  private final Slugify slugify = Slugify.builder().build();
 
-    @Test
-    public void shouldReturnSlugifiedString() {
-        // given
-        final String text = "Hello, world!";
+  @Test
+  public void shouldReturnSlugifiedString() {
+    // given
+    final String text = "Hello, world!";
 
-        // when
-        final String result = slugify.slugify(text);
+    // when
+    final String result = slugify.slugify(text);
 
-        // then
-        assertEquals("hello-world", result);
-    }
+    // then
+    assertEquals("hello-world", result);
+  }
 
-    @Test
-    public void shouldTrimWhiteSpacesOtherThanSpace() {
-        // given
-        String string = "\tHello \tworld \r\t";
+  @Test
+  public void shouldTrimWhiteSpacesOtherThanSpace() {
+    // given
+    String string = "\tHello \tworld \r\t";
 
-        // when
-        String result = slugify.slugify(string);
+    // when
+    String result = slugify.slugify(string);
 
-        // then
-        assertEquals("hello-world", result);
-    }
+    // then
+    assertEquals("hello-world", result);
+  }
 
 
-    @Test
-    public void shouldSlugifyAnyPrintableASCIICharacter() {
-        // given
-        String string = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+  @Test
+  public void shouldSlugifyAnyPrintableAsciiCharacter() {
+    // given
+    String string = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`"
+        + "abcdefghijklmnopqrstuvwxyz{|}~";
 
-        // when
-        String result = slugify.slugify(string);
+    // when
+    String result = slugify.slugify(string);
 
-        // then
-        assertEquals("0123456789-abcdefghijklmnopqrstuvwxyz-_-abcdefghijklmnopqrstuvwxyz", result);
-    }
+    // then
+    assertEquals("0123456789-abcdefghijklmnopqrstuvwxyz-_-abcdefghijklmnopqrstuvwxyz", result);
+  }
 
-    @Test
-    public void shouldReplacePlusSignToSeparator() {
-        // given
-        String string = "\tHello+\tworld \r\t";
+  @Test
+  public void shouldReplacePlusSignToSeparator() {
+    // given
+    String string = "\tHello+\tworld \r\t";
 
-        // when
-        String result = slugify.slugify(string);
+    // when
+    String result = slugify.slugify(string);
 
-        // then
-        assertEquals("hello-world", result);
-    }
+    // then
+    assertEquals("hello-world", result);
+  }
 
-    @Test
-    public void shouldReturnEmptyStringIfNullGiven() {
-        // given
-        String string = null;
+  @Test
+  public void shouldReturnEmptyStringIfNullGiven() {
+    // given
+    String string = null;
 
-        // when
-        String result = slugify.slugify(string);
+    // when
+    String result = slugify.slugify(string);
 
-        // then
-        assertEquals("", result);
-    }
+    // then
+    assertEquals("", result);
+  }
 
-    @Test
-    public void shouldNormalizeRepeatedHyphensToSingleHyphenWithHyphenSeparator() {
-        //given
-        String string = "a---b___c";
+  @Test
+  public void shouldNormalizeRepeatedHyphensToSingleHyphenWithHyphenSeparator() {
+    //given
+    String string = "a---b___c";
 
-        //when
-        String result = slugify.slugify(string);
+    //when
+    String result = slugify.slugify(string);
 
-        //then
-        assertEquals("a-b___c", result);
-    }
+    //then
+    assertEquals("a-b___c", result);
+  }
 }
